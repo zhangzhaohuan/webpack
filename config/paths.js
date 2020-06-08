c90936448d91c038a93cb92f85fe47658aed689d
zhangzhaohuan <zhangzhaohuan@zhangzhaohuandeMacBook-Pro.local>
2020-06-08 10:44:59 +0800

    add注释

--- config/paths.js
+++ config/paths.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const fs = require('fs');
 
-const appDirectory = fs.realpathSync(process.cwd());
+const appDirectory = fs.realpathSync(process.cwd());  //cwd是指当前node命令执行时所在的文件夹目录；
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 
 
