874a5caceab34b39c867c349fde84dc9caa43888
zhangzhaohuan <zhangzhaohuan@aixuexi.com>
2020-08-14 10:00:11 +0800

    docs: 修改配置文件

--- config/paths.js
+++ config/paths.js
@@ -5,7 +5,9 @@ const fs = require('fs');
 
 const appDirectory = fs.realpathSync(process.cwd());  //cwd是指当前node命令执行时所在的文件夹目录；
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
-
+console.log('process.cwd():'+ process.cwd());
+console.log('appDirectory:'+ appDirectory);
+console.log(appDirectory);
 
 module.exports = {
   resolveApp:resolveApp
